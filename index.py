57db79e2c43b3af1476a4689ebdfdc2e4de7bc6d
mRcfps <1402491442@qq.com>
2018-04-22 14:41:35 +0800

    Add isSigned judgment from traceData

--- index.py
+++ index.py
@@ -1,53 +1,66 @@
-from flask import Flask, render_template, request, Response
 import json
+from flask import Flask, render_template, request, Response
+
 app = Flask(__name__, static_url_path="/assets", static_folder="assets")
 
 with open('traceData.json') as fp:
     trace_data = json.load(fp)
 
 humans = [
-        {
-            "name": "Yvonne",
-            "isSigned": False,
-            "avatar": "weige.jpg",
-            "activityData": trace_data
-        },
-        {
-            "name": "pftom",
-            "isSigned": False,
-            "avatar": "weige.jpg",
-            "activityData": trace_data
-        },
-        {
-            "name": "小哲哲",
-            "isSigned": False,
-            "avatar": "weige.jpg",
-            "activityData": trace_data
-        },
-        {
-            "name": "datangzong",
-            "isSigned": False,
-            "avatar": "weige.jpg",
-            "activityData": trace_data
-        },
+    {
+        "name": "Yvonne",
+        "isSigned": False,
+        "avatar": "weige.jpg",
+        "activityData": trace_data
+    },
+    {
+        "name": "pftom",
+        "isSigned": False,
+        "avatar": "weige.jpg",
+        "activityData": trace_data
+    },
+    {
+        "name": "小哲哲",
+        "isSigned": False,
+        "avatar": "weige.jpg",
+        "activityData": trace_data
+    },
+    {
+        "name": "datangzong",
+        "isSigned": False,
+        "avatar": "weige.jpg",
+        "activityData": trace_data
+    },
 ]
+
 
 def make_response_json(content):
     resp = Response(content)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
+
 @app.route('/')
 def index():
     return render_template('index.html', humans=humans)
 
+
 @app.route('/personProfile/')
 def personProfile():
     name = request.args.get('name')
+
+    # NOTE: now_human may be None!
     for human in humans:
         if human["name"] == name:
             now_human = human
+
+    # Check whether this person has signed
+    for record in trace_data:
+        if record['name'] == now_human['name']:
+            now_human['isSigned'] = True
+
     return render_template('personProfile.html', human=now_human)
+
 
 @app.route('/echarts/')
 def echarts():
